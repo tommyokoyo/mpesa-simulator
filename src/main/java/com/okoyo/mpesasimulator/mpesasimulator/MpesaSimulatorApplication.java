fc0a63b537d6ff4e4f615456132202245a6b4877
tommyokoyo <okoyotommy@gmail.com>
2025-01-08 17:44:38 +0300

    First commit

---
+++ src/main/java/com/okoyo/mpesasimulator/mpesasimulator/MpesaSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.okoyo.mpesasimulator.mpesasimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpesaSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpesaSimulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/okoyo/mpesasimulator/mpesasimulator/MpesaSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okoyo.mpesasimulator.mpesasimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpesaSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
